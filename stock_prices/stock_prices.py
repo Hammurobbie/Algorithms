5da780394fda33dfef787f8a9c0fa014b31ab7a4
Robbie Porter <rthomporter@gmail.com>
2020-04-16 15:34:40 -0500

    eating cookies + stocks almost complete

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,7 +4,30 @@ import argparse
 
 
 def find_max_profit(prices):
-    print("you ran the bih")
+
+    cur_max = 0
+    profit = 0
+
+    for i in prices:
+        for j in prices:
+            if j > cur_max and prices.index(j) > prices.index(i):
+                cur_max = j
+                dif = cur_max - i
+                if dif > profit:
+                    profit = dif
+        #     cur_min = i
+        # if i > cur_max:
+        #     cur_max = i
+        # elif i < cur_min:
+        #     cur_min = i
+        #     if prices.index(cur_min) > prices.index(cur_max):
+        #         print("fuck")
+
+    print(f'max dif {cur_max}')
+    print(f"A profit of {profit} can be made from the stock prices {prices}.")
+
+
+find_max_profit([250, 1230, 100, 500, 2500, 50, 1000])
 
 
 # if __name__ == '__main__':
@@ -17,5 +40,3 @@ def find_max_profit(prices):
 
 #     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
 #         profit=find_max_profit(args.integers), prices=args.integers))
-
-find_max_profit([250, 1230, 100, 500, 2500, 50, 1000])
